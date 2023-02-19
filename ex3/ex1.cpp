404074e2fff91dd69a571df7ac92c050d4ba71d8
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-02-19 22:18:29 +0200

    Add files via upload

---
+++ ex3/ex1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int year, days;
+
+    cout << "Enter the year number: ";
+    cin >> year;
+
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        days = 366;
+    }
+    else {
+        days = 365;
+    }
+
+    cout << "Number of days in a year - " << year << ": " << days << endl;
+
+    return 0;
+}
---
+++ ex3/ex2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int hryvnia, kopecks;
+
+	cout << "Enter the amount in UAH and kopecks: ";
+	cin >> hryvnia >> kopecks;
+
+	// коригування суми, якщо копійок більше 99
+	hryvnia += kopecks / 100;
+	kopecks %= 100;
+
+	cout << "Adjusted amount: " << hryvnia << " UAH " << kopecks << " cop" << endl;
+
+	return 0;
+}
