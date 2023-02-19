c2bb318f4d30e8c39d6fb6d303eba74745dffe1d
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-02-19 22:24:53 +0200

    Add files via upload

---
+++ ex3/ex5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float radius, volume;
+
+	cout << "Enter the radius of the sphere: ";
+	cin >> radius;
+
+	// обчислення об'єму кулі
+	volume = (4.0 / 3.0) * 3.14159 * radius * radius * radius;
+
+	cout << "Bullet volume: " << volume << endl;
+
+	return 0;
+}
