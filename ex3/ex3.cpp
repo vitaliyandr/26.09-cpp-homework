0eee299d77c351a68ecf063a54d213034981fb8d
Vitaliy <andrusishin.vit@gmail.com>
2023-02-19 22:17:11 +0200

    Добавьте файлы проекта.

---
+++ ex3/ex3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float length, width, height, volume;
+
+	cout << "Enter the length: ";
+	cin >> length;
+	cout << "Enter the width: ";
+	cin >> width;
+	cout << "Enter the height: ";
+	cin >> height;
+
+	// обчислення об'єму паралелепіпеда
+	volume = length * width * height;
+
+	cout << "The volume of a parallelepiped: " << volume << endl;
+
+	return 0;
+}
